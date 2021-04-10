ee9b12dcc0a92b05ac40202e38399d44d5bb5f94
Yannick Clement Gongue Gongue <82268062+YannickGongue@users.noreply.github.com>
2021-04-10 23:13:21 +0200

    connection to the Database

---
+++ clSetting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NCIProject
+{
+	public class clSetting
+	{
+		public static string connectionString()
+		{
+			string strConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;
+                                   Initial Catalog=DbProjectSub;Integrated Security=True";
+			return strConnectionString;
+		}
+	}
+}
